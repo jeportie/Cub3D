d7e1df5718b00f40d258c565eedf24df03e35b72
jeportie <jeportie@student.42.fr>
2025-01-27 01:39:37 +0000

    push pause

--- src/render/draw_wall_slice.c
+++ src/render/draw_wall_slice.c
@@ -6,7 +6,7 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 17:06:17 by jeportie          #+#    #+#             */
-/*   Updated: 2025/01/27 01:16:37 by jeportie         ###   ########.fr       */
+/*   Updated: 2025/01/27 01:33:10 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ int	draw_wall_slice(t_ray *ray, t_rndr_ctx *ctx, t_image *img)
 			put_pixel_to_image(img, ray->x_screen, y + ray->line_offset, BLACK);
 		}
 		else if ((int)ray->current_wall != ctx->prev_wall
-			|| abs(ctx->old_wall_height - ray->wall_height) > 20)
+			|| abs(ctx->old_wall_height - ray->wall_height) > 50)
 		{
-			if (ctx->old_wall_height - ray->wall_height > 20)
+			if (ctx->old_wall_height - ray->wall_height > 50)
 			{
 				is_incomplete = 1;
 				put_pixel_to_image(img, ray->x_screen, y + ray->line_offset,
